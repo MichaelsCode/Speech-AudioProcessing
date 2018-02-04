1220ff72eb8a430c814747b37e09ba59f260b13e
Michael Hernandez <michael.hernandez.solorzano@gmail.com>
2018-02-04 20:36:13 +0000

    Commit with Sine wave draft

--- lab1.py
+++ lab1.py
@@ -27,3 +27,27 @@ plt.show()
 
 #print(np.max(a1[1]))
 #print(np.argmax(a1[1]))
+
+f = 2 #frequency of the signal
+fs = 100 #sample rate
+
+t = np.arange(-1,1+1/fs,1/fs)
+A = np.sin(t)
+
+plt.plot(t,A)
+
+plt.title('Sine Wave')
+plt.xlabel('Time')
+
+plt.ylabel('Amplitude = sin(time)')
+
+plt.grid(True,which='both')
+
+plt.axhline(y=0, color='k')
+
+plt.show()
+
+plt.sho()
+
+
+
