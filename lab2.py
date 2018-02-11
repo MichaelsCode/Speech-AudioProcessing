071ee507e2673475afad0be93b5afde9ad9868d2
Michael Hernandez <michael.hernandez.solorzano@gmail.com>
2018-02-11 23:49:08 +0000

    Commit including updated first part of the lab 2

--- lab2.py
+++ lab2.py
@@ -2,20 +2,18 @@ import matplotlib.pyplot as plt
 import numpy as np
 import librosa
 import scipy.signal
-
+import scipy.io.wavfile
 
 #s ine wave formula A sin(wt)
-import result as result
 
-f = 2 # frequency of the signal
-fs = 770 # sample rate
 
-t = np.arange(-1,1+1/fs,1/fs) # time calculation
+fs = 44100 # sampling rate
+f = 770 # frequency of the signal
+dt = 0.5/fs
 
-A = np.sin(2*np.pi*f*t)# amplitude calculation
+t = np.arange(fs)*dt #time calculation (x axis)
 
-sinewave1 = np.array((t,A))
-print(sinewave1.shape)
+A =np.sin(2 * np.pi * f * t)  # amplitude calculation (y axis)
 
 plt.grid(axis='both') # draw grid on plot
 plt.plot(t,A) # plotting amplitude A(y axis), time t(y axis)
@@ -24,21 +22,16 @@ plt.title('Sine Wave') #plot title
 plt.xlabel('Time(s)', fontsize=16)#plot label for x axis
 
 plt.ylabel('Amplitude', fontsize=16)#plot label for y axis
-plt._show() #show plot on IDE
+#plt._show() #show plot on IDE
 
 #second signal
 
-f2 = 1
-fs2= 1209
+fs2 = 44100
+f2= 1209
+dt2 = 1/fs
 
-t2 = np.arange(-1,1+1/fs2,1/fs2)
+t2 = np.arange(fs2) * dt2
 A2 = np.sin(2*np.pi*f2*t2)
-
-sinewave2 = np.array((t2,A2))
-print(sinewave2.shape)
-
-
-print(plt.plot(t2,A2))
 
 plt.grid(axis='both') # draw grid on plot
 plt.plot(t2,A2) # plotting amplitude A(y axis), time t(y axis)
@@ -47,11 +40,44 @@ plt.title('Sine Wave 2') #plot title
 plt.xlabel('Time(s)', fontsize=16)#plot label for x axis
 
 plt.ylabel('Amplitude', fontsize=16)#plot label for y axis
-plt._show() #show plot on IDE
+#plt._show() #show plot on IDE
 
-#x =  np.arange(-1,1+1/fs,1/fs) + np.arange(-1,1+1/fs2,1/fs2)
-#y = np.sin(2*np.pi*f*t) + np.sin(2*np.pi*f2*t2)
+#I tried using append but without success
+result = A + A2 # mix sine waves
 
-#plt.plot(x,y)
+plt.plot((t+t2),result) # plot the sine wave
+plt.show()
 
+Fs = 8000
+
+#define the keypad and its frequencies
+validkeys = '*#0123456789ABCD'
+rowfreqs = [697, 770, 852, 941]
+colfreqs = [1209, 1336, 1477, 1633]
+buttons = {'1':(0,0), '2':(0,1), '3':(0,2), 'A':(0,3),
+'4':(1,0), '5':(1,1), '6':(1,2), 'B':(1,3),
+'7':(2,0), '8':(2,1), '9':(2,2), 'C':(2,3),
+'*':(3,0), '0':(3,1), '#':(3,2), 'D':(3,3)}
+
+def dtmf_encoder(phonenumber):
+
+    dur = 0.5
+    silencedur = 0.1
+    Fs= 8000
+
+
+    t = np.linspace(0, dur, int(dur * Fs), endpoint=False)
+    silence = np.zeros(int(silencedur * Fs))
+
+    sounds = []
+    for key in phonenumber:
+        if key.upper() in validkeys:
+            r, c = buttons[key]
+    fr, fc = rowfreqs[r], colfreqs[c]
+    # print key, fr, fc
+    sounds.append(np.sin(2 * np.pi * fr * t) + np.sin(2 * np.pi * fc * t))
+    sounds.append(silence)
+    np.concatenate(sounds[:-1])  # drop last silence period
+    #librosa.output.write_wav('C:/Users/Michael Hernandez/Desktop/sounds.wav', sounds, Fs)
+    return  np.concatenate(sounds[:-1])
 
