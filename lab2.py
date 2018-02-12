41e6a93d07e81ed1cd247b8ba274c1e5c5bc6735
Michael Hernandez <michael.hernandez@storyful.com>
2018-02-12 15:51:46 +0000

    Commit including the not working DTMF function

--- lab2.py
+++ lab2.py
@@ -52,34 +52,30 @@ plt.show()
 
 
 #define the keypad and its frequencies
-validkeys = '*#0123456789ABCD'
-rowfreqs = [697, 770, 852, 941]
-colfreqs = [1209, 1336, 1477, 1633]
+validkeys = '*#0123456789ABCD' #valid keys
+rowfreqs = [697, 770, 852, 941] #row frequencies
+colfreqs = [1209, 1336, 1477, 1633] #column frequencies
 buttons = {'1':(0,0), '2':(0,1), '3':(0,2), 'A':(0,3),
 '4':(1,0), '5':(1,1), '6':(1,2), 'B':(1,3),
 '7':(2,0), '8':(2,1), '9':(2,2), 'C':(2,3),
-'*':(3,0), '0':(3,1), '#':(3,2), 'D':(3,3)}
+'*':(3,0), '0':(3,1), '#':(3,2), 'D':(3,3)} #matrix of buttons
 
+# I could not make this code to work I kept getting the following error: sounds = np.append([np.sin(2 * np.pi * fr * t).astype(np.float32) + np.sin(2 * np.pi * fc * t).astype(np.float32)],axis=0)
+#TypeError: append() missing 1 required positional argument: 'values'
 def dtmf_encoder(phonenumber):
 
     dur = 0.5
-    silencedur = 0.1
-    Fs= 44100
-
+    Fs= 44100 # sampling rate
 
     t = np.linspace(0, dur, dur * Fs, endpoint=False)
-    silence = np.zeros(int(silencedur * Fs))
 
-    sounds = np.zeros()
+    sounds = np.zeros(int(phonenumber)) #array of zeros where I intended to append signals
+
     for key in phonenumber:
         if key.upper() in validkeys:
             r, c = buttons[key]
             fr, fc = rowfreqs[r], colfreqs[c]
-    # print key, fr, fc
     sounds = np.append(np.sin(2 * np.pi * fr * t).astype(np.float32) + np.sin(2 * np.pi * fc * t).astype(np.float32))
-    sounds = np.append(silence)
-    np.concatenate(sounds[:-1])  # drop last silence period
-    librosa.output.write_wav('C:/Users/Michael Hernandez/Desktop/sounds.wav', sounds, Fs)
-    return  np.concatenate(sounds[:-1])
+    return librosa.output.write_wav('C:/Users/Michael Hernandez/Desktop/sounds.wav', sounds, Fs)
 
-dtmf_encoder('12')
+
