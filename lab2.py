d2a568f205781ba38d8cc0ec5677baaa961d7818
MichaelsCode <michael.hernandez.solorzano@gmail.com>
2018-02-19 12:25:35 +0000

    Commit with function changed and mixed sine wave

--- lab2.py
+++ lab2.py
@@ -7,18 +7,18 @@ import scipy
 from scipy import signal
 import scipy.io.wavfile
 
-#s ine wave formula A sin(wt)
+'#s ine wave formula A sin(wt)'
 
 
 fs = 44100 # sampling rate
 f = 770 # frequency of the signal
-t = np.linspace(0, 0.5, fs) #time calculation (x axis)
+t = np.linspace(0, 0.5, fs, endpoint=True) #time calculation (x axis)
 
 A =np.sin(2 * np.pi * f * t).astype(np.float32)  # amplitude calculation (y axis)
 
 plt.grid(axis='both') # draw grid on plot
-plt.plot(t,A) # plotting amplitude A(y axis), time t(y axis)
 plt.axis([0,0.02,-1,1])
+plt.plot(t,A) # plotting amplitude A(y axis), time t(y axis)
 plt.title('Sine Wave') #plot title
 plt.xlabel('Time(s)', fontsize=16)#plot label for x axis
 
@@ -30,12 +30,12 @@ plt._show() #show plot on IDE
 fs2 = 44100
 f2= 1209
 
-t2 = np.linspace(0, 1, fs2)
+t2 = np.linspace(0, 1, fs2, endpoint=True)
 A2 = np.sin(2*np.pi*f2*t2)
 
 plt.grid(axis='both') # draw grid on plot
+plt.axis([0,0.02,-1,1])W
 plt.plot(t2,A2) # plotting amplitude A(y axis), time t(y axis)
-plt.axis([0,0.02,-1,1])
 
 plt.title('Sine Wave 2') #plot title
 plt.xlabel('Time(s)', fontsize=16)#plot label for x axis
@@ -45,17 +45,17 @@ plt._show() #show plot on IDE
 
 #Signals mixed
 times = np.array(t + t2)
-result = np.array(np.sin(2 * np.pi * f * t).astype(np.float32) + np.sin(2 * np.pi * f2*t2).astype(np.float32))
-librosa.output.write_wav('/Users/michael/Desktop/result.wav', result, fs)
+result = np.array(A + A2)
+sine_wave_mixed = np.array([result,times])
+librosa.output.write_wav('/home/michael/Documents/sinewavemixed.wav', result, fs)
 
 #result = A + A2 # mix sine waves
-
-
+plt.grid(axis='both') # draw grid on plot
+#plt.axis([0,result.size,-1,1])
+plt.plot(times,result) # plotting amplitude A(y axis), time t(y axis)
 plt.title('Sine Wave Mixed') #plot title
 plt.xlabel('Time(s)', fontsize=16)#plot label for x axis
-
 plt.ylabel('Amplitude', fontsize=16)#plot label for y axis
-plt.plot(result) # plot the sine wave; could not plot the mixed sine wave at 0.02
 plt.show()
 
 #define the keypad and its frequencies
@@ -78,12 +78,11 @@ def dtmf_encoder(phonenumber):
         if key.upper() in validkeys:
             r, c = buttons[key]
             fr, fc = rowfreqs[r], colfreqs[c]
-    sounds = np.array(np.sin(2 * np.pi * fr * t).astype(np.float32) + np.sin(2 * np.pi * fc * t).astype(np.float32))
-    #librosa.output.write_wav('/home/michael/Documents/sounds.wav', sounds, Fs)
-    librosa.output.write_wav('/Users/michael/Desktop/sounds.wav', sounds, Fs)
+    sounds = np.array(np.cos(2 * np.pi * fr * t).astype(np.float32) + np.cos(2 * np.pi * fc * t).astype(np.float32))
+    librosa.output.write_wav('/home/michael/Documents/sounds.wav', sounds, Fs)
     return
 
-dtmf_encoder('4') #run the function
+dtmf_encoder('5') #run the function
 
 #Tone of digit three
 
@@ -100,9 +99,9 @@ col_A =np.sin(2 * np.pi * col_f * row_t).astype(np.float32)
 
 digit3 = np.array(np.sin(2 * np.pi * row_f * row_t).astype(np.float32) + np.sin(2 * np.pi * col_f * row_t).astype(np.float32))
 
-librosa.output.write_wav('/Users/michael/Desktop/digit3.wav', digit3, fs)
+librosa.output.write_wav('/home/michael/Documents/digit3.wav', digit3, fs)
 
-b, a = signal.butter(6, 1000, 'low', analog=True,output='ba') #here I'm using a filter order 6 and cutoff at 1000
+b, a = signal.butter(6, 1000/(fs/2), 'low', analog=True,output='ba') #here I'm using a filter order 6 and cutoff at 1000
 
 row_A, col_A = signal.freqs(b, a) #here I'm applying the butterworth filter.
 
@@ -117,4 +116,4 @@ plt.axvline(1000, color='green') # cutoff frequency
 plt.show()
 
 signal_filtered = np.array(row_A,col_A)
-librosa.output.write_wav('/Users/michael/Desktop/digit3filtered.wav', signal_filtered, fs)
+#librosa.output.write_wav('/home/michael/Documents/digit3filtered.wav', signal_filtered, fs)
