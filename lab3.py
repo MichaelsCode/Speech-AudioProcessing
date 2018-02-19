51780bb43f01d9da0f172b23b6d66a0d9f66d90e
MichaelsCode <michael.hernandez.solorzano@gmail.com>
2018-02-19 13:08:22 +0000

    Commit including first part of the lab 3

--- lab3.py
+++ lab3.py
@@ -2,17 +2,23 @@ import matplotlib.pylab as plt
 import numpy as np
 from scipy.fftpack import fft
 
+fs = 44100
 
-Fs = 10000
-Ts = 1.0/Fs
+f = 10000
 
-t = np.arange(0,1,Ts)
+Ts = 1/fs
 
-ff = 1
+t = np.arange(0, 1, Ts)
 
-y = np.sin(2*np.pi*ff*t)
+A = np.sin(2*np.pi * f * t)
 
-plt.plot(t,y)
+n = len(A) # length of the signal
+
+k = np.arange(n)
+
+T = n / fs
+
+plt.plot(t,A)
 
 plt.title('Sine Wave')
 plt.xlabel('Time(s)', fontsize=16)
@@ -22,6 +28,22 @@ plt.ylabel('Amplitude', fontsize=16)
 plt.grid(True,which='both')
 
 plt.axhline(y=0, color='k')
+frq = k/T # two sides frequency range
+
+frq = frq[range(int(n/2))] # one side frequency range
+
+Y = np.fft.fft(A)/n #fft computing and normalization
+
+Y = Y[range(int(n/2))]
+
+fig, ax = plt.subplots(2,1)
+
+ax[0].plot(t,A)
+ax[0].set_xlabel('Time')
+ax[0].set_ylabel('Amplitude')
+ax[1].plot(frq,abs(Y),'r') # plotting the spectrum
+ax[1].set_xlabel('Freq (Hz)')
+ax[1].set_ylabel('|Y(freq)|')
 
 plt.show()
 
