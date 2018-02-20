1596976471d172576eb5208d5d3ee50d9933ff7e
Michael Hernandez <michael.hernandez.solorzano@gmail.com>
2018-02-20 19:04:58 +0000

    Commit including updated first part of the lab 2

--- lab3.py
+++ lab3.py
@@ -1,6 +1,8 @@
 import matplotlib.pylab as plt
 import numpy as np
-from scipy.fftpack import fft
+from scipy import signal
+from scipy.fftpack import fft, fftshift
+from scipy.signal import get_window
 
 fs = 44100
 
@@ -14,25 +16,21 @@ A = np.sin(2*np.pi * f * t)
 
 n = len(A) # length of the signal
 
+'''
+n = 256
+n = 512
+n = 1024
+'''
+
 k = np.arange(n)
 
-T = n / fs
+T = n/fs
 
-plt.plot(t,A)
-
-plt.title('Sine Wave')
-plt.xlabel('Time(s)', fontsize=16)
-
-plt.ylabel('Amplitude', fontsize=16)
-
-plt.grid(True,which='both')
-
-plt.axhline(y=0, color='k')
 frq = k/T # two sides frequency range
 
 frq = frq[range(int(n/2))] # one side frequency range
 
-Y = np.fft.fft(A)/n #fft computing and normalization
+Y = fft(A)/n #fft computing and normalization
 
 Y = Y[range(int(n/2))]
 
@@ -41,10 +39,17 @@ fig, ax = plt.subplots(2,1)
 ax[0].plot(t,A)
 ax[0].set_xlabel('Time')
 ax[0].set_ylabel('Amplitude')
-ax[1].plot(frq,abs(Y),'r') # plotting the spectrum
+ax[1].plot(frq,np.abs(Y),'r') # plotting the spectrum
 ax[1].set_xlabel('Freq (Hz)')
 ax[1].set_ylabel('|Y(freq)|')
 
 plt.show()
 
 
+'''
+plt.plot(window)
+plt.title("Hamming window")
+plt.ylabel("Amplitute")
+plt.xlabel("Sample")
+plt.show()
+'''
